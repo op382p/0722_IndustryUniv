3f7bd8505d1ee11e016f6c88fdd59cccb1752655
op382p <op382p@naver.com>
2021-07-22 15:07:59 +0900

    0722_IndustryUniv

---
+++ script/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $(".mainLi a").mouseover(function(){
+        $(".subMenu").stop().slideDown(800);
+        $("div#subMenuBG").stop().slideDown(800);
+    });
+
+    $(".mainLi a").mouseout(function(){
+        $(".subMenu").stop().slideUp(300);
+        $("div#subMenuBG").stop().slideUp(300);
+    });
+
+
+});
